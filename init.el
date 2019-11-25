5ca6cce2a897016b9bae8043ffad0a7cb4705248
Mauricio Téllez <mauricio.tellez@gmail.com>
2019-11-25 00:17:35 -0600

    Define el valor de custom-file.
    Agrega la definición de los paquetes:
      - helm-projectile.

--- init.el
+++ init.el
@@ -1,8 +1,32 @@
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
 (use-package projectile
   :ensure t
+  :defer t
   :init (setq projectile-completion-system 'helm)
   (setq projectile-indexing-method 'alien)
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1)
-  (helm-projectile-on))
+  (projectile-mode +1))
+
+(use-package helm-projectile
+  :ensure t
+  :defer t
+  :after (projectile)
+  :config (helm-projectile-on))
